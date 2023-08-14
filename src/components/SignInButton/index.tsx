d5d5bd1ad38ce472acd0ef1062f338dd28522725
EngJao89 <eng_joao@outlook.com>
2023-08-14 18:07:59 -0300

    fix(in24-user-fix): corrige login de usuário

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -5,7 +5,7 @@ import { signIn, signOut, useSession } from 'next-auth/react'
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const [session] = useSession()
+  const session = useSession()
 
   return session ? (
     <button 
