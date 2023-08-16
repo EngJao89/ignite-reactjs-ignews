b1d0e6dd461ad2abe5530ebdfe20b6daed2a7c6f
EngJao89 <eng_joao@outlook.com>
2023-08-16 19:15:28 -0300

    feat(in28-home-ajuste): implementa textos e estilização de home

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,9 +1,20 @@
 import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  return (
+  const isUserLoggedIn = true;
+  return isUserLoggedIn ? (
+    <button 
+      type="button"
+      className={styles.signInButton}
+    >
+      <FaGithub color="#04d361" />
+      João Ricardo
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
     <button 
       type="button"
       className={styles.signInButton}
@@ -11,5 +22,5 @@ export function SignInButton() {
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,26 @@
+import Head from 'next/head';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
-    <h1>IgNews</h1>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for R$9,99 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
   )
 }
